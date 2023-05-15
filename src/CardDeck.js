f51f9675268bce9d42f5207251b68eae63c1fbde
Rajayne <ryann.j.nealey@gmail.com>
2023-05-15 14:30:05 -0700

    Create remaining cards state that removes draw card button at 0

--- src/CardDeck.js
+++ src/CardDeck.js
@@ -5,43 +5,45 @@ import "./CardDeck.css";
 
 const CardDeck = () => {
   const cardURL = "https://deckofcardsapi.com/api/deck";
-  const [deckId, setDeckId] = useState("");
+  const [deck, setDeck] = useState("");
   const [cards, setCards] = useState([]);
+  const [remaining, setRemaining] = useState(52);
 
   useEffect(() => {
     async function requestDeck() {
       const res = await axios.get(`${cardURL}/new/shuffle`);
-      setDeckId(res.data.deck_id);
+      setDeck(res.data);
     }
     requestDeck();
   }, []);
 
   async function drawCard(e) {
     e.preventDefault();
-    const res = await axios.get(`${cardURL}/${deckId}/draw`);
-    console.log(res.data.cards[0].image);
+    const res = await axios.get(`${cardURL}/${deck.deck_id}/draw`);
+    setRemaining((remaining) => remaining - 1);
     setCards([...cards, res.data.cards[0].image]);
   }
 
-  async function shuffleDeck(e) {
+  async function resetDeck(e) {
     e.preventDefault();
-    await axios.get(`${cardURL}/${deckId}/shuffle/`);
+    await axios.get(`${cardURL}/${deck.deck_id}/shuffle/`);
     setCards([]);
+    setRemaining(52);
     alert("Deck Reset!");
   }
 
   return (
     <div className="CardDeck">
       <h1 className="CardDeck-Header">Card Deck</h1>
-      <p>{deckId}</p>
-      {deckId !== "" ? (
+      <p>{remaining}</p>
+      {remaining !== 0 ? (
         <button className="CardDeck-button" onClick={drawCard}>
           Draw a Card!
         </button>
       ) : (
-        <h1>"Loading..."</h1>
+        ""
       )}
-      <button onClick={shuffleDeck}>Reset Deck!</button>
+      <button onClick={resetDeck}>Reset Deck!</button>
       <div className="CardDeck-Cardwrapper">
         {cards.map((card) => (
           <Card card={card} />
