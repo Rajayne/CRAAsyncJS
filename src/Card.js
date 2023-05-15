2df0e880878f0938e9a4705e1559011a76a3bba9
Rajayne <ryann.j.nealey@gmail.com>
2023-05-15 13:55:30 -0700

    Create card component and button that requests card image from current deck

---
+++ src/Card.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect, useRef } from "react";
+
+const Card = ({ card }) => {
+  return <img className="Card" src={card} alt=""></img>;
+};
+
+export default Card;
--- src/CardDeck.js
+++ src/CardDeck.js
@@ -1,9 +1,12 @@
 import React, { useState, useRef, useEffect } from "react";
 import axios from "axios";
+import Card from "./Card";
+import "./CardDeck.css";
 
 const CardDeck = () => {
-  const [deckId, setDeckId] = useState("");
   const cardURL = "https://deckofcardsapi.com/api/deck";
+  const [deckId, setDeckId] = useState("");
+  const [cards, setCards] = useState([]);
 
   useEffect(() => {
     async function requestDeck() {
@@ -13,12 +16,29 @@ const CardDeck = () => {
     requestDeck();
   }, []);
 
-  console.log(deckId);
+  async function handleClick(e) {
+    e.preventDefault();
+    const res = await axios.get(`${cardURL}/${deckId}/draw`);
+    console.log(res.data.cards[0].image);
+    setCards([...cards, res.data.cards[0].image]);
+  }
 
   return (
-    <div>
-      <h1>Card Deck</h1>
+    <div className="CardDeck">
+      <h1 className="CardDeck-Header">Card Deck</h1>
       <p>{deckId}</p>
+      {deckId !== "" ? (
+        <button className="CardDeck-button" onClick={handleClick}>
+          Draw a Card!
+        </button>
+      ) : (
+        <h1>"Loading..."</h1>
+      )}
+      <div className="CardDeck-Cardwrapper">
+        {cards.map((card) => (
+          <Card card={card} />
+        ))}
+      </div>
     </div>
   );
 };
