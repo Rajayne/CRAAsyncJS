e0e8648953af538d74858054d8239720c6528d33
Rajayne <ryann.j.nealey@gmail.com>
2023-05-15 14:17:21 -0700

    Create deck reset button

--- src/CardDeck.js
+++ src/CardDeck.js
@@ -10,17 +10,24 @@ const CardDeck = () => {
 
   useEffect(() => {
     async function requestDeck() {
-      const res = await axios.get(`${cardURL}/new`);
+      const res = await axios.get(`${cardURL}/new/shuffle`);
       setDeckId(res.data.deck_id);
     }
     requestDeck();
   }, []);
 
-  async function handleClick(e) {
+  async function drawCard(e) {
     e.preventDefault();
     const res = await axios.get(`${cardURL}/${deckId}/draw`);
     console.log(res.data.cards[0].image);
     setCards([...cards, res.data.cards[0].image]);
+  }
+
+  async function shuffleDeck(e) {
+    e.preventDefault();
+    await axios.get(`${cardURL}/${deckId}/shuffle/`);
+    setCards([]);
+    alert("Deck Reset!");
   }
 
   return (
@@ -28,12 +35,13 @@ const CardDeck = () => {
       <h1 className="CardDeck-Header">Card Deck</h1>
       <p>{deckId}</p>
       {deckId !== "" ? (
-        <button className="CardDeck-button" onClick={handleClick}>
+        <button className="CardDeck-button" onClick={drawCard}>
           Draw a Card!
         </button>
       ) : (
         <h1>"Loading..."</h1>
       )}
+      <button onClick={shuffleDeck}>Reset Deck!</button>
       <div className="CardDeck-Cardwrapper">
         {cards.map((card) => (
           <Card card={card} />
