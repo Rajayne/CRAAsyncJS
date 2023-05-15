584d4293c8da096be5c02f5c7487fc67d476b175
Rajayne <ryann.j.nealey@gmail.com>
2023-05-15 13:26:02 -0700

    Create async get request to API for new deckId and save to state

--- src/CardDeck.js
+++ src/CardDeck.js
@@ -1,9 +1,24 @@
 import React, { useState, useRef, useEffect } from "react";
+import axios from "axios";
 
 const CardDeck = () => {
+  const [deckId, setDeckId] = useState("");
+  const cardURL = "https://deckofcardsapi.com/api/deck";
+
+  useEffect(() => {
+    async function requestDeck() {
+      const res = await axios.get(`${cardURL}/new`);
+      setDeckId(res.data.deck_id);
+    }
+    requestDeck();
+  }, []);
+
+  console.log(deckId);
+
   return (
     <div>
       <h1>Card Deck</h1>
+      <p>{deckId}</p>
     </div>
   );
 };
